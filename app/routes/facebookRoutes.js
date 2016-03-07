b4b088a637e99f4a2bfd1818fb4e55ebd63ee1f1
Kenneth Massada <kmassada@videologygroup.com>
2016-03-06 20:34:10 -0500

    explicitly send token in header

--- app/routes/facebookRoutes.js
+++ app/routes/facebookRoutes.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var authRouter = express.Router();
-var jwt    = require('jsonwebtoken');
-var secret = process.env.SECRET;
+var apiPrefix = process.env.APP_API_VERSION;
 
 /* Handle Login POST */
 authRouter.route('/')
@@ -12,33 +11,21 @@ authRouter.route('/')
 
 /* Handle callback GET */
 authRouter.route('/callback')
-    .get(function(req, res, next) {
-      passport.authenticate('facebook', function(err, user) {
-        console.log(user);
-        if (err) {
-          return next(err);
-        }
-        // If user is found and password is right
-        // create a token
-        var token = jwt.sign(user, secret, {
-          expiresIn: 43200 * 60 * 60, // Expires in 24 hours
-        });
-
-        // Return the information including token as JSON
+    .get(passport.authenticate('facebook',
+      { session: false, failureRedirect: apiPrefix + '/auth/' }),
+      function(req, res) {
+        res.setHeader('x-access-token',  req.user.token);
         res.json({
           success: true,
           message: 'Enjoy your token!',
-          token: token,
-          user: user,
         });
-      })(req, res, next);
-    });
+      });
 
 /* Handle Logout */
 authRouter.route('/signout')
     .get(function(req, res) {
-      req.logout();
-      res.redirect('/');
+      req.user.token = '';
+      res.redirect(apiPrefix);
     });
 
 module.exports = authRouter;

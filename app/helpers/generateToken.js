daef1fc228166982d5d3ba9a76ad2608c8e8d7cc
Kenneth Massada <kmassada@videologygroup.com>
2016-03-06 20:32:38 -0500

    strip token from user before sending

--- app/helpers/generateToken.js
+++ app/helpers/generateToken.js
@@ -1,7 +1,10 @@
 var jwt    = require('jsonwebtoken');
+var User   = require('../models/user');
 var secret = process.env.SECRET;
 
 var generateToken = function generateToken(user) {
+  // Facebook token too long, stripping token
+  user.token = '';
   var token = jwt.sign(user, secret, {
       expiresIn: 43200 * 60 * 60, // Expires in 24 hours
     });
